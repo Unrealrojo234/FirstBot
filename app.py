0180933fcbea5d167597ab23340e17256a049596
Unrealrojo234 <otienotyan812@gmail.com>
2024-12-03 13:04:39 +0300

    Added CORS for access from all origins

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 import requests
+from flask_cors import CORS
 from dotenv import load_dotenv
 import os
 
@@ -9,6 +10,7 @@ load_dotenv()
 
 app = Flask(__name__)
 
+CORS(app)
 
 @app.route("/")
 def root():
